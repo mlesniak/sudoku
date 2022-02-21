15c189e538cfad5052748de2b866d9e4c4d2665f
Michael Lesniak <mail@mlesniak.com>
2022-02-21 20:39:42 +0100

    Check for completeness of a grid

--- src/main/kotlin/com/mlesniak/sudoku/Main.kt
+++ src/main/kotlin/com/mlesniak/sudoku/Main.kt
@@ -4,6 +4,7 @@ typealias Grid2D<T> = MutableList<MutableList<T>>
 
 // Contains a single 3x3 block.
 class Grid {
+    // IDEA: Use onedimensional list
     private val values: Grid2D<Int>
 
     init {
@@ -43,12 +44,31 @@ class Grid {
     fun set(x: Int, y: Int, value: Int) {
         values[y][x] = value
     }
+
+    fun complete(): Boolean {
+        val vals = values.flatten()
+        for (i in 1..9) {
+            // Valid if once or not at all
+            if (vals.count { it == i } != 1) {
+                return false
+            }
+        }
+        return true
+    }
 }
 
 fun main() {
     val g = Grid()
-    g.set(0, 0, 3)
-    g.set(1, 1, 4)
+    g.set(0, 0, 1)
+    g.set(1, 0, 2)
+    g.set(2, 0, 3)
+    g.set(0, 1, 4)
+    g.set(1, 1, 5)
+    g.set(2, 1, 6)
+    g.set(0, 2, 7)
+    g.set(1, 2, 8)
+    g.set(2, 2, 9)
     println(g)
     println(g.valid())
+    println(g.complete())
 }
