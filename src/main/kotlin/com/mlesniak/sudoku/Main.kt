0005cabf17d5bcc46f3319dda21028ebbd8c37c9
Michael Lesniak <mail@mlesniak.com>
2022-02-21 20:30:43 +0100

    Check for validity of grid configuration

--- src/main/kotlin/com/mlesniak/sudoku/Main.kt
+++ src/main/kotlin/com/mlesniak/sudoku/Main.kt
@@ -9,7 +9,7 @@ class Grid {
     init {
         val rows = mutableListOf<MutableList<Int>>()
         for (i in 0..2) {
-            rows.add(mutableListOf(-1, -1, -1))
+            rows.add(mutableListOf(0, 0, 0))
         }
         values = rows
     }
@@ -23,9 +23,32 @@ class Grid {
         sb.append(values[2])
         return sb.toString()
     }
+
+    fun valid(): Boolean {
+        // We assume that it's always a 3x3 grid.
+        val vals = values.flatten()
+        if (!vals.all { (it in 0..9) }) {
+            return false
+        }
+        for (i in 1..9) {
+            // Valid if once or not at all
+            if (vals.count { it == i } >= 2) {
+                return false
+            }
+
+        }
+        return true
+    }
+
+    fun set(x: Int, y: Int, value: Int) {
+        values[y][x] = value
+    }
 }
 
 fun main() {
     val g = Grid()
+    g.set(0, 0, 3)
+    g.set(1, 1, 4)
     println(g)
+    println(g.valid())
 }
