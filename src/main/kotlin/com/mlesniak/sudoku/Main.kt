a5bc23de1af50823f724abd317c7c0114e40a95f
Michael Lesniak <mail@mlesniak.com>
2022-02-21 20:20:18 +0100

    Add basic 3x3 grid

--- src/main/kotlin/com/mlesniak/sudoku/Main.kt
+++ src/main/kotlin/com/mlesniak/sudoku/Main.kt
@@ -1,12 +1,31 @@
 package com.mlesniak.sudoku
 
-class App {
-    val greeting: String
-        get() {
-            return "Hello World!"
+typealias Grid2D<T> = MutableList<MutableList<T>>
+
+// Contains a single 3x3 block.
+class Grid {
+    private val values: Grid2D<Int>
+
+    init {
+        val rows = mutableListOf<MutableList<Int>>()
+        for (i in 0..2) {
+            rows.add(mutableListOf(-1, -1, -1))
         }
+        values = rows
+    }
+
+    override fun toString(): String {
+        val sb = StringBuilder()
+        sb.append(values[0])
+        sb.append("\n")
+        sb.append(values[1])
+        sb.append("\n")
+        sb.append(values[2])
+        return sb.toString()
+    }
 }
 
 fun main() {
-    println(App().greeting)
+    val g = Grid()
+    println(g)
 }
