6018d8c56ea5f01a7f0386a4905bbf620249530b
Michael Lesniak <mail@mlesniak.com>
2022-02-28 07:43:40 +0100

    Collecting ideas

--- src/main/kotlin/com/mlesniak/sudoku/Main.kt
+++ src/main/kotlin/com/mlesniak/sudoku/Main.kt
@@ -2,14 +2,15 @@ package com.mlesniak.sudoku
 
 import java.io.File
 
-// IDEA(mlesniak) Nicer interface?
-class Sudoku(private val values: IntArray = IntArray(9 * 9) { 0 }) {
+class Sudoku(
+    private val grid: IntArray = IntArray(9 * 9) { 0 }
+) {
     override fun toString(): String {
         val sb = StringBuilder()
 
         for (y in 0 until 9) {
             for (x in 0 until 9) {
-                val v = values[y * 9 + x]
+                val v = grid[y * 9 + x]
                 sb.append(v)
                 sb.append(' ')
                 if (x % 3 == 2) {
@@ -25,12 +26,10 @@ class Sudoku(private val values: IntArray = IntArray(9 * 9) { 0 }) {
         return sb.toString()
     }
 
-    // just checks that the non-0 values are ok
-    // TODO Implement this
-    fun valid(): Boolean {
+    private fun valid(): Boolean {
         // Rows
         for (row in 0..8) {
-            val rowValues = values.filterIndexed { index, _ -> index / 9 == row }
+            val rowValues = grid.filterIndexed { index, _ -> index / 9 == row }
             val counts = rowValues
                 .groupBy { it }
                 .filter { it.key != 0 }
@@ -43,7 +42,7 @@ class Sudoku(private val values: IntArray = IntArray(9 * 9) { 0 }) {
 
         // Columns
         for (col in 0..8) {
-            val colValues = values.filterIndexed { index, _ -> index % 9 == col }
+            val colValues = grid.filterIndexed { index, _ -> index % 9 == col }
             val counts = colValues
                 .groupBy { it }
                 .filter { it.key != 0 }
@@ -81,10 +80,10 @@ class Sudoku(private val values: IntArray = IntArray(9 * 9) { 0 }) {
         return true
     }
 
-    fun complete(): Boolean = values.count { it > 0 } == 81
+    private fun complete(): Boolean = !grid.any { it == 0 }
 
     private fun get(x: Int, y: Int): Int {
-        return values[y * 9 + x]
+        return grid[y * 9 + x]
     }
 
     fun solve(): Sudoku? {
@@ -93,7 +92,7 @@ class Sudoku(private val values: IntArray = IntArray(9 * 9) { 0 }) {
         }
 
         // Find an unused field.
-        val unusedIndex = values
+        val unusedIndex = grid
             .mapIndexed { index, i -> if (i == 0) index else -1 }
             .firstOrNull { it >= 0 }
             ?: return null
@@ -101,7 +100,7 @@ class Sudoku(private val values: IntArray = IntArray(9 * 9) { 0 }) {
         // Iterate through all possible values and recurse.
         for (pv in 1..9) {
             // Create copy.
-            val copy = values.copyOf()
+            val copy = grid.copyOf()
             copy[unusedIndex] = pv
             val sc = Sudoku(copy)
             if (sc.valid()) {
@@ -115,20 +114,35 @@ class Sudoku(private val values: IntArray = IntArray(9 * 9) { 0 }) {
         return null
     }
 
+    /*
+
+        backtrack(v[1..k]) {
+            if v is a solution
+                report v
+            else
+                 for each promising choice of x
+                    backtrack(v[1..k];v[k+1]<-x)
+           }
+
+     */
+
     companion object {
         fun read(filename: String): Sudoku {
-            val lines = File(filename).readLines()
-            val input = lines.joinToString().filter { it.isDigit() }
-            val ints = input.map { it.toString().toInt() }.toIntArray()
-            return Sudoku(ints)
+            val initialValues = File(filename)
+                .readLines()
+                .joinToString()
+                .filter { it.isDigit() }
+                .map { it.toString().toInt() }
+                .toIntArray()
+            return Sudoku(initialValues)
         }
     }
 }
 
 fun main() {
-    val s = Sudoku.read("hard.txt")
-    // val s = Sudoku.read("example.txt")
-   // val s = Sudoku.read("solved-except-one.txt")
+//    val s = Sudoku.read("hard.txt")
+    val s = Sudoku.read("example.txt")
+    // val s = Sudoku.read("solved-except-one.txt")
     val solution = s.solve()
     println("Solution:")
     println(solution)
